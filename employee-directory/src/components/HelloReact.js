f6864536f6052641275a40282700d88cf811e415
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-07-06 14:10:31 -0400

    adding basic bootstrap

---
+++ employee-directory/src/components/HelloReact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HelloReact() {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Employee Directory</h1>
+        <p>I'm a jumbotron!</p>
+        <p>
+          <button className="btn btn-primary btn-lg">Learn more</button>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default HelloReact;
